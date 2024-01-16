16e5fd3014bb076fb4811ddfbbe10848c18572c1
yuemengrui <1401190335@qq.com>
2024-01-16 10:38:56 +0800

    fix bug

--- PaimonGPT/info/utils/kb/prompt_handler.py
+++ PaimonGPT/info/utils/kb/prompt_handler.py
@@ -1,7 +1,7 @@
 # *_*coding:utf-8 *_*
 # @Author : YueMengRui
 from configs.prompt_template import kb_qa_prompt_template, tableQA_prompt_template
-from info.mysql_models import App, KBFile, KBFileChunks, KB
+from info.mysql_models import App, KBFile, KBFileChunks, KB, App_KB
 from info.utils.kb.Retriever import multiquery_retriever, bm25_retriever
 
 
@@ -13,22 +13,24 @@ def get_kb_data(mysql_db, app_id):
     llm_name = None
 
     app = mysql_db.query(App).get(app_id)
-    if app and app.kb_id:
+    app_kbs = mysql_db.query(App_KB).filter(App_KB.app_id == app_id).all()
+    if app and app_kbs:
         llm_name = app.llm_name
-        kb = mysql_db.query(KB).get(app.kb_id)
-        if kb:
-            embedding_model = kb.embedding_model
-            kb_file_list = mysql_db.query(KBFile).filter(KBFile.kb_id == app.kb_id, KBFile.is_delete == False).all()
-            if kb_file_list:
-                for kb_file in kb_file_list:
-                    data_chunks = mysql_db.query(KBFileChunks).filter(KBFileChunks.data_id == kb_file.id).all()
-                    if data_chunks:
-                        for c in data_chunks:
-                            if c.type == 'text' and c.content_hash is not None:
-                                if c.content_hash not in text_hash_list:
-                                    data_chunk_map.update({c.content_hash: kb_file.to_dict()})
-                                    text_hash_list.append(c.content_hash)
-                                    text_list.append(c.content)
+        for app_kb in app_kbs:
+            kb = mysql_db.query(KB).get(app_kb.kb_id)
+            if kb:
+                embedding_model = kb.embedding_model
+                kb_file_list = mysql_db.query(KBFile).filter(KBFile.kb_id == app_kb.kb_id, KBFile.is_delete == False).all()
+                if kb_file_list:
+                    for kb_file in kb_file_list:
+                        data_chunks = mysql_db.query(KBFileChunks).filter(KBFileChunks.data_id == kb_file.id).all()
+                        if data_chunks:
+                            for c in data_chunks:
+                                if c.type == 'text' and c.content_hash is not None:
+                                    if c.content_hash not in text_hash_list:
+                                        data_chunk_map.update({c.content_hash: kb_file.to_dict()})
+                                        text_hash_list.append(c.content_hash)
+                                        text_list.append(c.content)
 
     return data_chunk_map, text_hash_list, text_list, embedding_model, llm_name
 
